c6f9edf89ece14c150eb2c314b79f1cd5da5d930
Maxim <85259443+Severmax@users.noreply.github.com>
2025-07-07 06:37:32 +0300

    commit

--- main.dart.js
+++ main.dart.js
@@ -63283,7 +63283,7 @@ l(){var s=this.d
 s===$&&A.a()
 s.l()
 this.Qq()},
-P(a){var s,r,q=null,p=A.cI(a,q,t.w).w,o=t.E,n=A.c([A.aej("../assets/hero_bg.png",B.f2,B.iT,q)],o)
+P(a){var s,r,q=null,p=A.cI(a,q,t.w).w,o=t.E,n=A.c([A.aej("assets/hero_bg.png",B.f2,B.iT,q)],o)
 if(p.a.a<600)n.push(A.h8(q,q,B.B,A.au(B.c.ac(76.5),B.m.v()>>>16&255,B.m.v()>>>8&255,B.m.v()&255),q,q,q,q,q,q,q,q,q))
 p=this.e
 p===$&&A.a()
@@ -63301,7 +63301,7 @@ bv(){this.dh()
 this.cI()
 this.lA()}}
 A.tz.prototype={
-aj(){return new A.I6(A.aoH(1.5),A.c([new A.pg("../assets/icons/premium.png","\u041f\u043e\u0446\u0456\u043d\u043e\u0432\u0443\u0432\u0430\u0447\u0456\u0432 \u043f\u0440\u0435\u043c\u0456\u0430\u043b\u044c\u043d\u0438\u0445 \u0440\u0435\u0447\u0435\u0439"),new A.pg("../assets/icons/celebration.png","\u041e\u0441\u043e\u0431\u043b\u0438\u0432\u0438\u0445 \u0441\u0432\u044f\u0442 \u0456 \u0432\u0430\u0436\u043b\u0438\u0432\u0438\u0445 \u043f\u043e\u0434\u0456\u0439"),new A.pg("../assets/icons/mind.png","\u0420\u043e\u0437\u0432\u0438\u0442\u043a\u0443 \u043c\u0438\u0441\u043b\u0435\u043d\u043d\u044f \u0456 \u0442\u0435\u0440\u043f\u0456\u043d\u043d\u044f \u0443 \u0432\u0438\u0448\u0443\u043a\u0430\u043d\u043e\u043c\u0443 \u0441\u0442\u0438\u043b\u0456")],t.pd))}}
+aj(){return new A.I6(A.aoH(1.5),A.c([new A.pg("assets/icons/premium.png","\u041f\u043e\u0446\u0456\u043d\u043e\u0432\u0443\u0432\u0430\u0447\u0456\u0432 \u043f\u0440\u0435\u043c\u0456\u0430\u043b\u044c\u043d\u0438\u0445 \u0440\u0435\u0447\u0435\u0439"),new A.pg("assets/icons/celebration.png","\u041e\u0441\u043e\u0431\u043b\u0438\u0432\u0438\u0445 \u0441\u0432\u044f\u0442 \u0456 \u0432\u0430\u0436\u043b\u0438\u0432\u0438\u0445 \u043f\u043e\u0434\u0456\u0439"),new A.pg("assets/icons/mind.png","\u0420\u043e\u0437\u0432\u0438\u0442\u043a\u0443 \u043c\u0438\u0441\u043b\u0435\u043d\u043d\u044f \u0456 \u0442\u0435\u0440\u043f\u0456\u043d\u043d\u044f \u0443 \u0432\u0438\u0448\u0443\u043a\u0430\u043d\u043e\u043c\u0443 \u0441\u0442\u0438\u043b\u0456")],t.pd))}}
 A.I6.prototype={
 aq(){this.aU()
 this.f=A.aqI(B.z7,new A.a5k(this))},
